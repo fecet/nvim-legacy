5f75a5aff0be6c19ec5f451ff15d7ea0efa87967
fecet <xiezej@gmail.com>
2022-04-26 14:07:28 +0800

    :wastebasket: ensure_installed=all

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,7 +25,7 @@ vim.cmd("set foldmethod=expr")
 vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = {}, -- List of parsers to ignore installing
     indent = {
         enable = true
