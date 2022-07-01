d5d03f0b4a06f367e5c6e592faa69cedd63a46e5
fecet <xiezej@gmail.com>
2022-07-01 22:49:25 +0800

    add logo for dashboard

--- lua/dashboard1.lua
+++ lua/dashboard1.lua
@@ -53,3 +53,38 @@ g.dashboard_custom_header = {
 }
 
 g.dashboard_disable_statusline = 1
+
+local home = os.getenv('HOME')
+local db = require('dashboard')
+db.preview_command = 'cat | lolcat -F 0.3'
+-- db.preview_command = 'lolcat'
+db.preview_file_path = home .. '/.config/nvim/neovim.cat'
+db.preview_file_height = 12
+db.preview_file_width = 80
+db.custom_center = {
+    {icon = '  ',
+    desc = 'Recently opened files                   ',
+    shortcut = 'SPC s l',
+    action ='SessionLoad'},
+    {icon = '  ',
+    desc = 'Recently opened files                   ',
+    action =  'DashboardFindHistory',
+    shortcut = 'SPC f h'},
+    {icon = '  ',
+    desc = 'Find  File                              ',
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+    shortcut = 'SPC f f'},
+    {icon = '  ',
+    desc ='File Browser                            ',
+    action =  'Telescope file_browser',
+    shortcut = 'SPC f b'},
+    {icon = '  ',
+    desc = 'Find  word                              ',
+    action = 'Telescope live_grep',
+    shortcut = 'SPC f w'},
+    {
+        icon = '  ',
+    desc = 'Open Personal dotfiles                  ',
+    action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+    shortcut = 'SPC f d'},
+  }
