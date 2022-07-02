ef8ab87dff1c4fb36343f5710a0b71c034e5fa0b
fecet <xiezej@gmail.com>
2022-07-02 23:13:58 +0800

    upd telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,6 +26,6 @@ override_file_sorter = true,
 	-- require('telescope').load_extension('session-lens')
 	require("telescope").load_extension("fzy_native")
 	-- require("telescope").load_extension("dotfiles")
-	require("telescope").load_extension("gosource")
+	-- require("telescope").load_extension("gosource")
 	require("telescope").load_extension("file_browser")
 end
