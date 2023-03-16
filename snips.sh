4b95cb498570ad502ccceb57a4e4129a7b730f2b
fecet <xiezej@gmail.com>
2023-03-16 15:57:12 +0800

    snips

---
+++ snips.sh
@@ -0,0 +1,5 @@
+rm -rf /home/rok/.local/share/nvim/site/pack/packer/start/vim-snippets
+rm -rf /home/rok/.local/share/nvim/site/pack/packer/opt/luasnips-mathtex-snippets
+ln -sf "$HOME/Nutstore Files/codes/luasnips-mathtex-snippets" /home/rok/.local/share/nvim/site/pack/packer/opt/luasnips-mathtex-snippets
+ln -sf "$HOME/Nutstore Files/codes/vim-snippets" /home/rok/.local/share/nvim/site/pack/packer/start/vim-snippets
+
